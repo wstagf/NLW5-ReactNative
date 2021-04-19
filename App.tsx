7ca1134533ba1964833e9bfadb9c4e4f5db942ec
Thiago Augusto Gonçalves Fernandes Santos Silva <wstagf@gmail.com>
2021-04-19 09:29:07 -0300

    testando expogo, visualizar a aplicação no iphone pela wifi

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World ReactNative</Text>
+      <Text>Hello World ReactNative - Wifi</Text>
       <StatusBar style="auto" />
     </View>
   );
