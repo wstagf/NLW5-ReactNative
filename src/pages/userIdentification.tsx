6ee02693746ad933ff8ac9a2445699dac9eabac4
Thiago Augusto Gonçalves Fernandes Santos Silva <wstagf@gmail.com>
2021-04-21 13:38:44 -0300

    adicionado icone e texto na pagina identificacao

--- src/pages/userIdentification.tsx
+++ src/pages/userIdentification.tsx
@@ -7,6 +7,7 @@ import {
     TouchableOpacity,
     Dimensions,
     View,
+    TextInput,
 } from 'react-native'
 
 import colors from '../styles/colors'
@@ -19,6 +20,10 @@ export function UserIdentification() {
             <View style={style.content}>
                 <View style={style.form}>
                     <Text style={style.emoji}>😀</Text>
+                    <Text style={style.title}>
+                        Como podemos {'\n'} chamar você?
+                    </Text>
+                    <TextInput style={style.input} />
                 </View>
             </View>
         </SafeAreaView>
@@ -32,7 +37,32 @@ const style = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'space-around',
     },
-    content: {},
-    form: {},
-    emoji: {},
+    content: { flex: 1, width: '100%' },
+    form: {
+        flex: 1,
+        justifyContent: 'center',
+        paddingHorizontal: 54,
+        alignItems: 'center',
+    },
+    emoji: {
+        fontSize: 44,
+    },
+    input: {
+        borderBottomWidth: 1,
+        borderColor: colors.green,
+        color: colors.heading,
+        width: '100%',
+        fontSize: 18,
+        marginTop: 50,
+        padding: 10,
+        textAlign: 'center',
+    },
+    title: {
+        fontSize: 28,
+        textAlign: 'center',
+        color: colors.heading,
+        lineHeight: 32,
+        marginTop: 20,
+        fontFamily: fonts.heading,
+    },
 })
