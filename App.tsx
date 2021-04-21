613a1b3ad99c9bd04ecb2b7b00d1d7156fa93df3
Thiago Augusto Gonçalves Fernandes Santos Silva <wstagf@gmail.com>
2021-04-21 09:51:49 -0300

    carregando fontes atravez do google fonts

--- App.tsx
+++ App.tsx
@@ -1,10 +1,30 @@
-import React from 'react';
-import { Text, View, StyleSheet} from 'react-native';
-import { Welcome } from './src/pages/welcome';
-
+import React from 'react'
+import { View, Text } from 'react-native'
+import { Welcome } from './src/pages/welcome'
+import {
+    useFonts,
+    Jost_400Regular,
+    Jost_600SemiBold,
+} from '@expo-google-fonts/jost'
 
 export default function App() {
-  return (
-    <Welcome></Welcome>
-  )
+    const [fontsLoaded] = useFonts({
+        Jost_400Regular,
+        Jost_600SemiBold,
+    })
+
+    if (!fontsLoaded) {
+        return (
+            <View
+                style={{
+                    flex: 1,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                }}>
+                <Text>Carregando Fontes...</Text>
+            </View>
+        )
+    } else {
+        return <Welcome></Welcome>
+    }
 }
