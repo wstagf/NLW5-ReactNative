3b5c24022b0dd2b444500e63bb9633ee354c01e9
Thiago Augusto Gonçalves Fernandes Santos Silva <wstagf@gmail.com>
2021-04-21 09:30:18 -0300

    corrigido formatacao do layout da welcome

--- src/pages/welcome.tsx
+++ src/pages/welcome.tsx
@@ -1,63 +1,76 @@
-import React, {useState} from 'react';
-import { Text, SafeAreaView, StyleSheet, Image} from "react-native";
+import React, { useState } from 'react'
+import {
+    Text,
+    SafeAreaView,
+    StyleSheet,
+    Image,
+    TouchableOpacity,
+    View,
+} from 'react-native'
 
 import wateringImg from '../assets/watering.png'
-import { Button } from '../components/button';
-import colors from '../styles/colors';
+import colors from '../styles/colors'
 
 export function Welcome() {
-
-    const [visible, setVisible] = useState(true);
-
-    function handleVisibitityTrue() {
-        setVisible(true);
-    }
-
-    function handleVisibitityFalse() {
-        setVisible(false);
-    }
-
-    return(
+    return (
         <SafeAreaView style={style.container}>
-            <Text style={style.title}>Gerencie {'\n'} suas plantas  {'\n'}  de forma fácil</Text>
-            {
-                visible && <Image source={wateringImg} style={style.image}></Image>
-            }            
-            <Text style={style.subtitle}>Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar você
-sempre que precisar.</Text>
-            {/* <Button title="Mostrar Imagem" onPress={handleVisibitityTrue}></Button>
-            <Button title="Ocultar Imagem" onPress={handleVisibitityFalse}></Button> */}
-            <Button title=">"></Button>
+            <Text style={style.title}>
+                Gerencie {'\n'} suas plantas {'\n'} de forma fácil
+            </Text>
+
+            <Image source={wateringImg} style={style.image}></Image>
+
+            <Text style={style.subtitle}>
+                Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar
+                você sempre que precisar.
+            </Text>
+
+            <TouchableOpacity style={style.button}>
+                <Text style={style.buttonText}>{'>'}</Text>
+            </TouchableOpacity>
         </SafeAreaView>
     )
 }
 
-const style = StyleSheet.create(
-    {
-      container: {
+const style = StyleSheet.create({
+    container: {
         flex: 1,
-        justifyContent: 'space-between',
-        alignItems: 'center', 
-      },
-      title: {
-          fontSize: 28,
-          fontWeight: 'bold',
-          textAlign: 'center',
-          color: colors.heading,
-          lineHeight: 32,
-          marginTop: 30
-      },
-      subtitle: {
+        alignItems: 'center',
+        justifyContent: 'space-around',
+    },
+    title: {
+        fontSize: 28,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        color: colors.heading,
+        lineHeight: 32,
+    },
+    subtitle: {
         textAlign: 'center',
         fontSize: 18,
         lineHeight: 25,
         paddingHorizontal: 20,
         fontWeight: '400',
-        color: colors.heading        
-      },
-      image: {
+        color: colors.heading,
+    },
+    image: {
         width: 150,
-        height: 120
-      },
-    }
-  )
+        height: 120,
+    },
+
+    button: {
+        backgroundColor: colors.green,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 16,
+        marginBottom: 10,
+        padding: 10,
+        height: 56,
+        minWidth: 56,
+    },
+
+    buttonText: {
+        color: colors.white,
+        fontSize: 24,
+    },
+})
